a262de92004dcfb16e4a5dfe8070bdde3df2d9c0
Kris Longmore <kris@robotwealth.com>
2020-10-26 10:56:20 +1100

    fix extra bracket

--- R/rwlab_gc_auth.R
+++ R/rwlab_gc_auth.R
@@ -23,7 +23,7 @@ rwlab_gc_auth <- function(oauth_email = NA, oauth_cache = TRUE) {
   if (length(Sys.glob("/usr/local/lib/python*/dist-packages/google/colab/_ipython.py")) > 0)
     # file.exists("/usr/local/lib/python3.6/dist-packages/google/colab/_ipython.py")) {
     R.utils::reassignInPackage("is_interactive", pkgName = "httr", function() return(TRUE))
-  }
+  # }
 
   stopifnot(interactive(), "Error: This function is for use in an interactive session.")
 
