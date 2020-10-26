08b403221384aad6371d8078f9e12a96bdea0188
Kris Longmore <kris@robotwealth.com>
2020-10-26 11:15:02 +1100

    force out of band auth for colab session

--- R/rwlab_gc_auth.R
+++ R/rwlab_gc_auth.R
@@ -20,12 +20,12 @@
 #' \dontrun{rwlab_gc_auth()}
 rwlab_gc_auth <- function(oauth_email = NA, oauth_cache = TRUE) {
 
-  if (length(Sys.glob("/usr/local/lib/python*/dist-packages/google/colab/_ipython.py")) > 0) {
+  if (length(Sys.glob("/usr/local/lib/python*/dist-packages/google/colab/_ipython.py")) > 0) {  # or interactive()
     print("colab session detected")
     # file.exists("/usr/local/lib/python3.6/dist-packages/google/colab/_ipython.py")) {
-    # R.utils::reassignInPackage("is_interactive", pkgName = "httr", function() return(TRUE))
+    R.utils::reassignInPackage("is_interactive", pkgName = "httr", function() return(TRUE))
     options(gargle_oob_default = TRUE)
-  }
+  } # else gargle_oob_default = FALSE
 
   # stopifnot(interactive(), "Error: This function is for use in an interactive session.")
 
