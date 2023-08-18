5d00c2fc2d75e3db335b54103db6ae0c1002f7d8
robotmasterkris <kris@robotwealth.com>
2023-08-18 11:15:45 +0800

    add assertthat dependency and bump version

--- examples/colab/load_libraries.R
+++ examples/colab/load_libraries.R
@@ -61,7 +61,7 @@ load_libraries <- function(load_rsims = TRUE, extra_libraries = c(), extra_depen
   # rwRtools dependencies (install but don't load)
   rwRtools_dependencies <- c(
     "pillar", "tibble", "rlang", "httr", "iterators", "zoo", "R.methodsS3",
-    "callr", "foreach", "xts", "stringi", "Rcpp", "R.oo", "gargle",
+    "callr", "foreach", "xts", "stringi", "Rcpp", "R.oo", "gargle", "assertthat",
     "googleAuthR", "glue", "googleCloudStorageR", "R.utils", "feather", "arrow",
     "lubridate", "readr", "stringr", "dplyr", "purrr", "magrittr", "TTR", "doParallel"
   )
